500fad71b49fd6a19e0053ac42ab15777280e91e
Charlton Wilbur <cwilbur@chromatico.net>
2015-05-01 09:22:05 -0400

    Initial commit.

---
+++ exercises/exercise-3.js
@@ -0,0 +1,20 @@
+// Exercise 3
+//
+// 1. Building on Exercise 2, implement the following methods:
+//
+// age() - returns the age of the car.
+// setYear()
+// getYear()
+// setMake()
+// getMake()
+// setModel()
+// getModel()
+// setColor()
+// getColor()
+// Include validity checking for setYear().
+
+// 2. Modify the constructor you have written so that the mileage is
+//    passed in as a parameter and recorded as an attribute.
+
+// 3.  Add accessors for the new mileage.  For validity, ensure that the
+//     mileage for a car can never be decreased.
