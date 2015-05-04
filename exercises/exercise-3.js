600316d7433c8f6f6a7f76f2d3cc2c6c6335225f
Charlton Wilbur <cwilbur@chromatico.net>
2015-05-04 16:26:51 -0400

    Add one more exercise, clean up the format

--- exercises/exercise-3.js
+++ exercises/exercise-3.js
@@ -1,8 +1,9 @@
+
 // Exercise 3
 //
-// 1. Building on Exercise 2, implement the following methods:
+// 1. Building on Exercise 2, implement the
+// following methods:
 //
-// age() - returns the age of the car.
 // setYear()
 // getYear()
 // setMake()
@@ -13,8 +14,25 @@
 // getColor()
 // Include validity checking for setYear().
 
-// 2. Modify the constructor you have written so that the mileage is
-//    passed in as a parameter and recorded as an attribute.
+// 2. Modify the constructor you have written so
+// that the mileage is passed in as a parameter
+// and recorded as an attribute.
 
-// 3.  Add accessors for the new mileage.  For validity, ensure that the
-//     mileage for a car can never be decreased.
+// 3.  Add accessors for the new mileage.  For
+// validity, ensure that the mileage for a car
+// can never be decreased.
+
+// 4. The database backing our list of cars only
+// recognizes seven colors, plus none.
+//
+// Make sure that when a car color is set, it is
+// in the list: red, orange, black, green, blue,
+// purple, and white.  "None" is also allowed and
+// should be the default.
+//
+// Modify the constructor so that if you pass it
+// an unrecognized color, it stores the string
+// "none" instead. If a programmer passes an
+// unrecognized color to the accessor function,
+// you should write a message to the console and
+// make no change to the object.
